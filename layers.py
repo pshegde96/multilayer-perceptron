328232e86c9ec3b017f606ad7869e278613adc3d
pshegde96 <parihegde96@gmail.com>
2017-08-22 00:29:57 +0530

    add layers.py;forward pass in layers complete

--- layers.py
+++ layers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import activations as act
 
 class layer:
 
@@ -10,4 +11,20 @@ class layer:
     #Initialize the weight matrix and the bias vector via Xavier Initialization
     def init_variables(self):
         self.W = 0.01*np.random.randn(self.in_dim,self.out_dim)/np.sqrt(self.in_dim)
-        self.b = np.zeros((1,self.out_dim))
+        self.b = 0.01*np.ones((1,self.out_dim)) #initialize with a small +ve value so that relu neurons don't go to 0 at birth
+
+    '''
+    The operation is A = f(Z)
+    Z = XW
+    '''
+    def forward(self,X):
+        self.Z = X.dot(self.W)+self.b
+        
+        if self.activation == 'linear':
+            self.A = self.Z
+        elif self.activation == 'sigmoid':
+            self.A = act.sigmoid_fn(self.Z)
+        else:
+            self.A = act.relu_fn(self.Z)
+
+        return self.A
