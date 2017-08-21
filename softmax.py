9b93ee2aba6419adf33566e6c561e92c64486649
pshegde96 <parihegde96@gmail.com>
2017-08-21 23:03:43 +0530

    softmax implemented; derivative yet to be implemented

---
+++ softmax.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def softmax(x,tmp=1):
+    big = np.max(x,axis=1)
+    x = x-big.reshape(-1,1)
+    print x
+    exp = np.exp(x*tmp)
+    print exp
+    return exp/(np.sum(exp,axis=1)).reshape(-1,1)
+
+if __name__ == "__main__":
+    main()
