f5360781a5d606d0b6f095588f9f43cc60360701
pshegde96 <parihegde96@gmail.com>
2017-08-25 19:28:30 +0530

    add cross_entropy.py; yet to verify its correctness

---
+++ cross_entropy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def cross_entropy(Y_hat,y,one_hot='True'):
+
+    if one_hot == 'False':
+        Y = np.zeros_like(Y_hat)
+        Y[range(y.shape[0]),y] = 1
+    else:
+        Y=y
+
+    inter = Y*np.log(Y_hat)
+    cross_entropy = -1/Y.shape[0]*(np.sum(inter))
+    return cross_entropy
