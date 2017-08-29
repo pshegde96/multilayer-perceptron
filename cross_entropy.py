de29b7beae959e057b3c7008a233ab0bafe918b0
pshegde96 <parihegde96@gmail.com>
2017-08-29 18:30:24 +0530

    fix scaling isssue in cross-entropy

--- cross_entropy.py
+++ cross_entropy.py
@@ -9,5 +9,5 @@ def cross_entropy(Y_hat,y,one_hot='True'):
         Y=y
 
     inter = Y*np.log(Y_hat+1e-7)
-    cross_entropy = -1/Y.shape[0]*(np.sum(inter))
+    cross_entropy = -1.0/Y.shape[0]*(np.sum(inter))
     return cross_entropy
